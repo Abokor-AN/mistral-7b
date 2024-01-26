9485e56dffd73da05835d5969a7943d4f6ea956c
Abokor Ahmed-Kadar Nour <155383356+Abokor-AN@users.noreply.github.com>
2024-01-26 22:17:25 +0300

    Add app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
+import torch
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
+
+model_name = "mistralai/Mistral-7B-Instruct-v0.2"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        load_in_4bit=True,
+        quantization_config=bnb_config,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+        trust_remote_code=True,
+    )
+
+pipe = pipeline(
+    "text-generation", 
+    model=model, 
+    tokenizer = tokenizer, 
+    torch_dtype=torch.bfloat16, 
+    device_map="auto"
+)
+
+prompt = "As a data scientist, can you explain the concept of regularization in machine learning?"
+
+sequences = pipe(
+    prompt,
+    do_sample=True,
+    max_new_tokens=100, 
+    temperature=0.7, 
+    top_k=50, 
+    top_p=0.95,
+    num_return_sequences=1,
+)
+print(sequences[0]['generated_text'])
