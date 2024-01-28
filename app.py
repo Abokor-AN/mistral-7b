2b225ea59ef307df333083c5e8dc41d44d7e231c
abokor-git <68802683+abokor-git@users.noreply.github.com>
2024-01-28 15:14:01 +0300

    Update

--- app.py
+++ app.py
@@ -1,41 +1,18 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
-import torch
+from langchain_community.llms import CTransformers
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+
+llm = CTransformers(
+    model='mistralai\Mistral-7B-Instruct-v0.2-GGUF\mistral-7b-instruct-v0.2.Q4_K_M.gguf', callbacks=[StreamingStdOutCallbackHandler()]
 )
 
-model_name = "mistralai/Mistral-7B-Instruct-v0.2"
+template = """Question: {question}
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        load_in_4bit=True,
-        quantization_config=bnb_config,
-        torch_dtype=torch.bfloat16,
-        device_map="auto",
-        trust_remote_code=True,
-    )
+Answer:"""
 
-pipe = pipeline(
-    "text-generation", 
-    model=model, 
-    tokenizer = tokenizer, 
-    torch_dtype=torch.bfloat16, 
-    device_map="auto"
-)
+prompt = PromptTemplate(template=template, input_variables=["question"])
+llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-prompt = "As a data scientist, can you explain the concept of regularization in machine learning?"
-
-sequences = pipe(
-    prompt,
-    do_sample=True,
-    max_new_tokens=100, 
-    temperature=0.7, 
-    top_k=50, 
-    top_p=0.95,
-    num_return_sequences=1,
-)
-print(sequences[0]['generated_text'])
+response = llm_chain.run("What is AI?")
